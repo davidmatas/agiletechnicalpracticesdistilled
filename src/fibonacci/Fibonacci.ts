fb038e98fd9630a1f31f30b024cfeea5a0d6e0d5
David Matas <adavidmatas@gmail.com>
2019-09-21 18:14:13 +0200

    Fibonacci and obvious implementation for first positions

---
+++ src/fibonacci/Fibonacci.ts
@@ -0,0 +1,6 @@
+export default class Fibonacci {
+  static at(position: number) {
+    if(position === 1) return 0
+    else return 1
+  }
+}
---
+++ src/fibonacci/__tests__/Fibonacci.test.ts
@@ -0,0 +1,27 @@
+import Fibonacci from '../Fibonacci';
+
+describe('Fibonacci', () => {
+  it('number at position 1 is 0', () => {
+    const position = 1;
+
+    const number = Fibonacci.at(position)
+
+    expect(number).toBe(0)
+  })
+
+  it('number at position 2 is 1', () => {
+    const position = 2;
+
+    const number = Fibonacci.at(position)
+
+    expect(number).toBe(1)
+  })
+
+  it('number at position 3 is 1', () => {
+    const position = 3;
+
+    const number = Fibonacci.at(position)
+
+    expect(number).toBe(1)
+  })
+})
